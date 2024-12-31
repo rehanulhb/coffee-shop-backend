f70effc5363fdc43d4fed6319ca6935d4e1bc78a
rehanulhb <rehanulh@gmail.com>
2024-12-31 19:35:59 +0100

    Create API for Saving User to the Database(MongoDB)

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('user')
 
     app.get('/coffee', async(req,res)=>{
       const cursor = coffeeCollection.find();
@@ -52,12 +53,42 @@ async function run() {
 
     })
 
+    app.put('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true};
+      const updatedCoffee = req.body;
+
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name, 
+          quantity: updatedCoffee.quantity, 
+          supplier: updatedCoffee.supplier, 
+          taste: updatedCoffee.taste, 
+          category: updatedCoffee.category, 
+          details: updatedCoffee.details, 
+          photo: updatedCoffee.photo
+
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    })
+
     app.delete('/coffee/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
+
+    //User API
+    app.post('/user', async(req, res)=>{
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
 
 
